c84ff03dea24bd916430705b492d47a085518f4a
muzixi <643170568@qq.com>
2019-01-23 14:02:23 +0800

    医生

--- emr/src/main/java/com/slwh/emr/controller/DoctorController.java
+++ emr/src/main/java/com/slwh/emr/controller/DoctorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-
+//医生doctor
 @RestController
 @RequestMapping("doctor")
 public class DoctorController {
